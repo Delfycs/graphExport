35e3375a0d7cd0e41a6b2cd9d9ffcc3285c0d96d
DELAGE Thomas <TDE@oresys.fr>
2015-12-29 13:33:35 +0100

    Initialisation

---
+++ app/index.js
@@ -0,0 +1,9 @@
+var i = 1;
+var u = 7;
+var w = 1;
+var toto = (function () {
+    function toto() {
+    }
+    return toto;
+})();
+var delfycs = { buque: "Delfycs", fams: 52 };
---
+++ app/index.ts
@@ -0,0 +1,16 @@
+var i = 1;
+
+const u = 7;
+
+let w = 1;
+
+class toto {
+
+}
+
+interface gadz {
+  buque: string;
+  fams: number;
+}
+
+var delfycs: gadz = { buque:"Delfycs", fams: 52 };
